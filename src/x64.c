278dbc70b11844748e9e552e2e28ebef4aa6e3f4
Yorn <yornclaudel@gmail.com>
2022-03-19 00:48:06 +0100

    working on finding symbols

--- src/x64.c
+++ src/x64.c
@@ -4,57 +4,61 @@
 #include "map_file.h"
 #include <stdio.h>
 
-Elf64_Shdr* find_strtab(Elf64_Shdr* s, uint16_t n)// TODO check if 2 strtab ?
+Elf64_Shdr* find_strtab(Elf64_Shdr* s, int16_t n)// TODO check if 2 strtab ?
 {
 	
-	while (n--)
+	for (int16_t i = 0; i < n; i++)
 	{
-		if (s->sh_type == SHT_STRTAB)
-			return s;
-		s += 1;
+		if (s[i].sh_type == SHT_STRTAB) //TODO manage multiple string tab
+		{
+			printf("STRTAB found at index : %u\n", i);
+			return &s[i];
+		}
 	}
 	return NULL;
 }
 
-
 int print_symbols(Elf64_Shdr* symtab, Elf64_Shdr* strtab, struct mfile* mf)
 {
 	// TODO check for alignement ?
-	uint64_t nsym = symtab->sh_size / symtab->sh_entsize;
+	int64_t nsym = symtab->sh_size / symtab->sh_entsize;
+	printf("entity size : %lu\n", symtab->sh_entsize);
 	Elf64_Sym* sentry = (Elf64_Sym*)(mf->data + symtab->sh_offset);
 	char *str;
 	while(nsym--)
 	{
-		printf("%lu\n", strtab->sh_offset);
 		str = (char*)(mf->data + strtab->sh_offset + sentry->st_name);
-		printf("%s\n", str);
+		if (sentry->st_name == 69)
+			printf("%s\n", str);
 		sentry += 1;
 	}
 	return 0;
 }
 
-#include <unistd.h>
-
 int read_ia64(struct mfile* mf)
 {
-	(void) mf;
 	Elf64_Ehdr* h = (Elf64_Ehdr*)mf->data;
 
-	uint64_t shstart = h->e_shoff; // section header offset
+	int64_t shstart = h->e_shoff; // section header offset
 	// TODO check if offset bigger than file ?
-	uint16_t n = h->e_shnum;
+	int16_t n = h->e_shnum;
 	printf("Section header is at offset %lu\n", shstart);
-	printf("There is %u sections\n", n);
+	printf("There is %d sections\n", n);
 
-	Elf64_Shdr* s = (Elf64_Shdr*)(mf->data + shstart);
+	Elf64_Shdr* s = (Elf64_Shdr*)&mf->data[shstart];
 	Elf64_Shdr* strtab = find_strtab(s, n);
 
-	// Reading sections 
-	while(n--)
+
+	for(int16_t i = 0; i < n; i++)
 	{
-		if (s->sh_type == SHT_SYMTAB)
-			print_symbols(s, strtab, mf);
-		s += 1;
+		printf("Type of section %x\n", s[i].sh_type);
+		if (s[i].sh_type == SHT_SYMTAB)
+		{
+
+			printf("Value of %x\n", s[i].sh_type);
+			print_symbols(&s[i], strtab, mf);
+		}
 	}
 	return 0;
 }
+
